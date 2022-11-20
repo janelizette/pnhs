53949ce782ec56ad89f8e29001b9a5bae6371adc
janelizette <janelizetteaguilar@gmail.com>
2022-11-20 10:59:02 +0800

    Add welcome page

--- php/connection.php
+++ php/connection.php
@@ -2,6 +2,6 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "pnhs-canteen";
-$con=mysqli_connect($server,$user,$password,$database) or die ("Error in connection");
-?>
+$database = "pnhs_canteen";
+// $database = "tbl_user";
+$con = mysqli_connect($server, $user, $password, $database) or die("Error in connection");
