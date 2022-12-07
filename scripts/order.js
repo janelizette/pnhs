40a27ed6c1d7e630c45c1ff930a4729e23ffe1ea
Ria Chico <chico.rializa@ue.edu.ph>
2022-12-07 16:28:16 +0800

    Added calculations

--- scripts/order.js
+++ scripts/order.js
@@ -1,7 +1,15 @@
 function plus() {
+  var quantity = parseInt(document.getElementById("quantity").value);
+  var prc = parseInt(document.getElementById("prc").value);
   document.getElementById("quantity").stepUp();
+  document.getElementById("total").innerHTML = "PHP " + ((quantity+1)*prc).toString();
 }
 
 function minus() {
+  var quantity = parseInt(document.getElementById("quantity").value);
+  var prc = parseInt(document.getElementById("prc").value);
   document.getElementById("quantity").stepDown();
+  if ((quantity-1) != 0) {
+    document.getElementById("total").innerHTML = "PHP " + ((quantity-1)*prc).toString();
+  }
 }
