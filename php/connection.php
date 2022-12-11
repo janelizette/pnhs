08ba195cba13ea147c62d3b9baf5ab8cb59774d3
Ria Chico <chico.rializa@ue.edu.ph>
2022-12-11 20:55:04 +0800

    Removed unnecessary comment

--- php/connection.php
+++ php/connection.php
@@ -3,5 +3,4 @@ $server = "localhost";
 $user = "root";
 $password = "";
 $database = "pnhs_canteen";
-// $database = "tbl_user";
 $con = mysqli_connect($server, $user, $password, $database) or die("Error in connection");
